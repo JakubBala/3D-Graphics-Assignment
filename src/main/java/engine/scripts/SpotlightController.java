4c2c84054d854e7917a72c4bae24ae00722d4950
Jakub Bala <63164657+JakubBala@users.noreply.github.com>
2025-12-10 09:05:46 +0000

    spotlight aiming logic

--- src/main/java/engine/scripts/SpotlightController.java
+++ src/main/java/engine/scripts/SpotlightController.java
@@ -2,6 +2,7 @@ package engine.scripts;
 
 import engine.components.Behaviour;
 import engine.components.Transform;
+import engine.gmaths.Vec3;
 
 
 // This should be attached to the arm pivot
@@ -22,5 +23,23 @@ public class SpotlightController extends Behaviour{
     @Override
     public void Update(){
 
+        Vec3 targetPos = new Vec3(target.GetWorldPosition()); 
+
+        Vec3 arm_pivot_w_pos = new Vec3(arm_pivot.GetWorldPosition());
+        Vec3 arm_pivot_target = new Vec3(targetPos.x, arm_pivot_w_pos.y, targetPos.z);
+
+        // for the arm pivot, we only want it to rotate on the X so we set a virtual target
+        // at the same Y level as it.
+        arm_pivot.LookAt(
+            arm_pivot_target,        
+            new Vec3(0, 1, 0)// world up vector
+        );
+
+
+        // then for the light itself, 
+        light_pivot.LookAt(
+            targetPos,        
+            new Vec3(0, 1, 0)// world up vector
+        );
     }
 }
